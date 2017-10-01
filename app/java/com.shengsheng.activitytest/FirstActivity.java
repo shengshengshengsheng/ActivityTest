2fccdcede9c4333453da2f2b18cc92a107510244
sheng <1591330057@qq.com>
2017-10-01 19:44:11 +0800

    Update FirstActivity.java

--- app/java/com.shengsheng.activitytest/FirstActivity.java
+++ app/java/com.shengsheng.activitytest/FirstActivity.java
@@ -2,6 +2,7 @@ package com.example.shengsheng.activitytest;
 
 import android.net.Uri;
 import android.support.annotation.LayoutRes;
+import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -24,8 +25,12 @@ public class FirstActivity extends AppCompatActivity {
             public void onClick(View v) {
             /*Toast.makeText(FirstActivity.this,"you click button1",Toast.LENGTH_SHORT).
             show();*/
-            Intent intent=new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http:www.baidu.com"));
+            String data="hello SecondActivity";
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+                //intent.putExtra("extra_data",data);
+             /*Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http:www.baidu.com"));*/
                 startActivity(intent);
         }
         });
@@ -49,6 +54,18 @@ public class FirstActivity extends AppCompatActivity {
         }
         return true;
     }
-
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        switch (requestCode)
+        {
+            case 1:if(resultCode==RESULT_OK)
+            {
+                String returnData=data.getStringExtra("data_return");
+                //Log.d("FirstActivity",returnedData);
+            }
+            break;
+            default:
+        }
+    }
 
 }
