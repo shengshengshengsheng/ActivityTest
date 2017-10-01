2bafd98b3649339635ca382f144a172f86a7a0ef
sheng <1591330057@qq.com>
2017-10-01 19:44:36 +0800

    Update SecondActivity.java

--- app/java/com.shengsheng.activitytest/SecondActivity.java
+++ app/java/com.shengsheng.activitytest/SecondActivity.java
@@ -1,5 +1,8 @@
 package com.example.shengsheng.activitytest;
-
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,22 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+        Button button2=(Button)findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent=new Intent();
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+
+        });
+
+        /*Intent intent=getIntent();
+        String data=intent.getStringExtra("extra_data");
+        Log.d("SecondActivity",data);*/
     }
 }
