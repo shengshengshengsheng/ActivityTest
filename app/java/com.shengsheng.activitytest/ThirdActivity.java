350bbd3de475a773a3de51541dbd2ebb6ff10e0b
sheng <1591330057@qq.com>
2017-10-02 14:00:40 +0800

    Update ThirdActivity.java

--- app/java/com.shengsheng.activitytest/ThirdActivity.java
+++ app/java/com.shengsheng.activitytest/ThirdActivity.java
@@ -2,12 +2,28 @@ package com.example.shengsheng.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class ThirdActivity extends AppCompatActivity {
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+public class ThirdActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Log.d("ThirdActivity","Task id is"+getTaskId());
         setContentView(R.layout.third_layout);
+        Button button3=(Button)findViewById(R.id.button_3);
+        button3.OnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ActivityCollector.finishall();
+
+            }
+        });
+
     }
 }
